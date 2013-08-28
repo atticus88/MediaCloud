6d0c75797d57e5c792a74e12d1af650723f877a0
Dustin Woodard <dustin.woodard@gmail.com>
2013-08-27 23:04:20 -0600

    added phpunit, artisan commands, app:install

--- app/config/app.php
+++ app/config/app.php
@@ -114,6 +114,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'Way\Generators\GeneratorsServiceProvider',
+		'Cartalyst\Sentry\SentryServiceProvider',
 
 	),
 
