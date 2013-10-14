61dc5de9d9ac106a79c83cdf8773e994c7c27840
Tanner Holm <holm.tanner@gmail.com>
2013-10-14 10:38:35 -0600

    edit asset form

--- app/views/backend/media_assets/edit.blade.php
+++ app/views/backend/media_assets/edit.blade.php
@@ -16,104 +16,160 @@
                      <div class="col-md-9">
                         <!-- <input type="text" class="form-control" placeholder="Chee Kin"> -->
                          {{ Form::text('title', $asset->title,  array('class' => 'form-control')) }}
-                        <span class="help-block">This is inline help</span>
+                        <!-- <span class="help-block">Give your Asset a title</span> -->
                     </div>
                 </div>
             </div>
             <!--/span-->
             <div class="col-md-6">
-              <div class="form-group has-error">
-                 <label class="control-label col-md-3">Last Name</label>
+              <div class="form-group">
+                 <label class="control-label col-md-3">Description</label>
                  <div class="col-md-9">
-                    <input type="text" class="form-control" placeholder="Lim">
-                    <span class="help-block">This field has error.</span>
+                     {{ Form::text('title', $asset->description,  array('class' => 'form-control')) }}
+                    <!-- <span class="help-block">Describe your Asset</span> -->
                 </div>
             </div>
         </div>
         <!--/span-->
     </div>
     <!--/row-->
+     <div class="row">
+               <div class="col-md-6">
+                  <div class="form-group">
+                     <label class="control-label col-md-3">Filepath</label>
+                     <div class="col-md-9">
+                        <!-- <input type="text" class="form-control" placeholder="Chee Kin"> -->
+                         {{ Form::text('title', $asset->title,  array('class' => 'form-control')) }}
+                        <!-- <span class="help-block">This is inline help</span> -->
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                  <div class="form-group">
+                     <label class="control-label col-md-3">Filename</label>
+                     <div class="col-md-9">
+                        <!-- <input type="text" class="form-control" placeholder="Chee Kin"> -->
+                         {{ Form::text('title', $asset->title,  array('class' => 'form-control')) }}
+                        <!-- <span class="help-block">This is inline help</span> -->
+                    </div>
+                </div>
+            </div>
+</div>
+   <div class="row">
+               <div class="col-md-6">
+                  <div class="form-group">
+                     <label class="control-label col-md-3">Transcoded URL</label>
+                     <div class="col-md-9">
+                        <!-- <input type="text" class="form-control" placeholder="Chee Kin"> -->
+                         {{ Form::text('title', $asset->title,  array('class' => 'form-control')) }}
+                        <!-- <span class="help-block">This is inline help</span> -->
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                  <div class="form-group">
+                     <label class="control-label col-md-3">Thumbnail URL</label>
+                     <div class="col-md-9">
+                        <!-- <input type="text" class="form-control" placeholder="Chee Kin"> -->
+                         {{ Form::text('title', $asset->title,  array('class' => 'form-control')) }}
+                        <!-- <span class="help-block">This is inline help</span> -->
+                    </div>
+                </div>
+            </div>
 </div>
 
-<ul>
-    <li class="lead">
-        {{ Form::label('title', 'Title',  array('class' => 'label label-primary')) }}
-        {{ Form::text('title', $asset->title,  array('class' => 'pull-right')) }}
-    </li>
+   <div class="row">
+               <div class="col-md-6">
+                  <div class="form-group">
+                     <label class="control-label col-md-3">URL</label>
+                     <div class="col-md-9">
+                        <!-- <input type="text" class="form-control" placeholder="Chee Kin"> -->
+                         {{ Form::text('title', $asset->title,  array('class' => 'form-control')) }}
+                        <!-- <span class="help-block">This is inline help</span> -->
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                  <div class="form-group">
+                     <label class="control-label col-md-3">Type</label>
+                     <div class="col-md-9">
+                        <!-- <input type="text" class="form-control" placeholder="Chee Kin"> -->
+                         {{ Form::text('title', $asset->title,  array('class' => 'form-control')) }}
+                        <!-- <span class="help-block">This is inline help</span> -->
+                    </div>
+                </div>
+            </div>
+</div>
+   <div class="row">
+               <div class="col-md-6">
+                  <div class="form-group">
+                     <label class="control-label col-md-3">Status</label>
+                     <div class="col-md-9">
+                        <!-- <input type="text" class="form-control" placeholder="Chee Kin"> -->
+                         {{ Form::text('title', $asset->title,  array('class' => 'form-control')) }}
+                        <!-- <span class="help-block">This is inline help</span> -->
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                  <div class="form-group">
+                     <label class="control-label col-md-3">Tags</label>
+                     <div class="col-md-9">
+                        <!-- <input type="text" class="form-control" placeholder="Chee Kin"> -->
+                         {{ Form::text('title', $asset->title,  array('class' => 'form-control')) }}
+                        <!-- <span class="help-block">This is inline help</span> -->
+                    </div>
+                </div>
+            </div>
+</div>
 
-    <li class="lead">
-        {{ Form::label('description', 'Description',  array('class' => 'label label-primary')) }}
-        {{ Form::text('description', $asset->description,  array('class' => 'pull-right')) }}
-    </li>
+   <div class="row">
+               <div class="col-md-6">
+                  <div class="form-group">
+                     <label class="control-label col-md-3">Views</label>
+                     <div class="col-md-9">
+                        <!-- <input type="text" class="form-control" placeholder="Chee Kin"> -->
+                         {{$asset->views}}
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                  <div class="form-group">
+                     <label class="control-label col-md-3">Last Viewed</label>
+                     <div class="col-md-9">
+                          {{$asset->last_viewed}}
+                    </div>
+                </div>
+            </div>
+</div>
 
-    <li class="lead">
-        {{ Form::label('filepath', 'Filepath',  array('class' => 'label label-primary')) }}
-        {{ Form::text('filepath', $asset->filepath,  array('class' => 'pull-right')) }}
-    </li>
+   <div class="row">
+               <div class="col-md-6">
+                  <div class="form-group">
+                     <label class="control-label col-md-3">Created At</label>
+                     <div class="col-md-9">
+                        <!-- <input type="text" class="form-control" placeholder="Chee Kin"> -->
+                        {{$asset->created_at}}
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                  <div class="form-group">
+                     <label class="control-label col-md-3">Updated At</label>
+                     <div class="col-md-9">
+                        <!-- <input type="text" class="form-control" placeholder="Chee Kin"> -->
+                      {{$asset->updated_at}}                       
+                    </div>
+                </div>
+            </div>
+</div>
 
-    <li class="lead">
-        {{ Form::label('filename', 'Filename',  array('class' => 'label label-primary')) }}
-        {{ Form::text('filename', $asset->filename,  array('class' => 'pull-right')) }}
-    </li>
 
-    <li class="lead">
-        {{ Form::label('transcoded_url', 'Transcoded_url',  array('class' => 'label label-primary')) }}
-        {{ Form::text('transcoded_url', $asset->transcoded_url,  array('class' => 'pull-right')) }}
-    </li>
 
-    <li class="lead">
-        {{ Form::label('thumbnail_url', 'Thumbnail_url',  array('class' => 'label label-primary')) }}
-        {{ Form::text('thumbnail_url', $asset->thumbnail_url,  array('class' => 'pull-right')) }}
-    </li>
 
-    <li class="lead">
-        {{ Form::label('url', 'Url',  array('class' => 'label label-primary')) }}
-        {{ Form::text('url', $asset->url,  array('class' => 'pull-right')) }}
-    </li>
-
-    <li class="lead">
-        {{ Form::label('type', 'Type:',  array('class' => 'label label-primary')) }}
-        {{ Form::text('type', $asset->type,  array('class' => 'pull-right')) }}
-    </li>
-
-    <li class="lead">
-        {{ Form::label('status', 'Status',  array('class' => 'label label-primary')) }}
-        {{ Form::text('status', $asset->status,  array('class' => 'pull-right')) }}
-    </li>
-
-    <li class="lead">
-        {{ Form::label('tags', 'Tags',  array('class' => 'label label-primary')) }}
-        {{ Form::text('tags', $asset->tags,  array('class' => 'pull-right')) }}
-    </li>
-
-    <li class="lead">
-        {{ Form::label('last_viewed', 'Last_viewed',  array('class' => 'label label-primary')) }}
-
-        {{$asset->last_viewed}}
-    </li>
-
-    <li class="lead">
-        {{ Form::label('views', 'Views', array('class' => 'label label-primary')) }}
-        {{$asset->last_viewed}}
-
-    </li>
-
-    <li class="lead">
-        {{ Form::label('created_at', 'Created_at', array('class' => 'label label-primary')) }}
-        {{$asset->last_viewed}}
-    </li>
-
-    <li class="lead">
-        {{ Form::label('updated_at', 'Updated_at', array('class' => 'label label-primary')) }}
-        {{$asset->last_viewed; array('class'=>'pull-left')}}
-    </li>
-
-    <li class="lead">
-     {{ Form::submit('Update', array('class' => 'btn btn-info')) }}
-     {{ link_to_route('assets', 'Cancel', array('class' => 'btn btn-info')) }}
- </li>
-</ul>
-
+ {{ Form::submit('Update', array('class' => 'btn-lg btn-info')) }}
+  
+   <button class="btn-lg btn-info" > {{ link_to_route('assets', 'Cancel') }}</button> 
 </form>
 
 
