cd0ef0eeb7b7ea01e97d645d5a4e1d5f3469227b
Dustin Woodard <dustinwoodard@weber.edu>
2013-12-02 17:31:32 -0700

    added validator class, rm blog views

---
+++ app/MC/Exceptions/ValidationException.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace MC\Exceptions;
+
+
+use Exception;
+
+class ValidationException extends Exception {
+
+    protected  $errors;
+    protected  $message;
+
+    public function __construct($message, $errors, $code=0, Exception $previous = null){
+
+        $this->errors = $errors;
+        $this->message = $message;
+
+        parent::__construct($message,$code,$previous);
+
+    }
+
+    public function getErrors(){
+        return $this->errors;
+    }
+} 
--- app/config/permissions.php
+++ app/config/permissions.php
@@ -6,78 +6,78 @@ return [
 		[
 			'permission' => 'admin',
 			'label'      => 'Admin Rights',
-		],
-		],
-		'Global' => [
-			[
-				'permission' => 'superuser',
-				'label'      => 'Super User',
 			],
 		],
-		'Frontend Admin' => [
+	'Global' => [
+		[
+			'permission' => 'superuser',
+			'label'      => 'Super User',
+			],
+		],
+	'Frontend Admin' => [
 		[
 			'permission' => 'superuser',
 			'label'      => 'Superuser Rights',
+			],
 		],
-	],
 
 	'User' => [
 		[
 			'permission' => 'user_getIndex',
 			'label'      => 'User Index',
-		],
+			],
 		[
 			'permission' => 'user_getCreate',
 			'label'      => 'User Create',
-		],
+			],
 		[
 			'permission' => 'user_postCreate',
 			'label'      => 'User Store',
-		],
+			],
 		[
 			'permission' => 'user_getEdit',
 			'label'      => 'User Show',
-		],
+			],
 		[
 			'permission' => 'user_postEdit',
 			'label'      => 'User Edit',
-		],
+			],
 		[
 			'permission' => 'user_getDelete',
 			'label'      => 'User Delete',
-		],
+			],
 		[
 			'permission' => 'user_getRestore',
 			'label'      => 'User Restore',
+			],
 		],
-	],
 	'Admin Asset' => [
 		[
 			'permission' => 'asset_getIndex',
 			'label'      => 'Asset Index',
-		],
+			],
 		[
 			'permission' => 'asset_getUpload',
 			'label'      => 'Asset Create',
-		],
+			],
 		[
 			'permission' => 'asset_postUpload',
 			'label'      => 'Asset Store',
-		],
+			],
 		[
 			'permission' => 'asset_getEdit',
 			'label'      => 'Asset Show',
-		],
+			],
 		[
 			'permission' => 'asset_postEdit',
 			'label'      => 'Asset Save',
-		],
+			],
 		[
 			'permission' => 'asset_getDelete',
 			'label'      => 'Asset Delete',
-		]
-	],
-];
+			]
+		],
+	];
 
 
 
