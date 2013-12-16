3b724492cba6be31eec8a6db0b63da28c93047b7
Dustin Woodard <dustin.woodard@gmail.com>
2013-12-16 10:29:05 -0700

    add install for vagrant

--- app/views/frontend/layouts/default.blade.php
+++ app/views/frontend/layouts/default.blade.php
@@ -5,7 +5,7 @@
 <!-- BEGIN HEAD -->
 <head>
 	<meta charset="utf-8" />
-	<title>MediaCloud</title>
+	<title>MediaCloud:test</title>
 	<meta content="width=device-width, initial-scale=1.0" name="viewport" />
 	<meta content="" name="description" />
 	<meta content="" name="author" />
